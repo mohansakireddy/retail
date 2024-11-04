782fc37f4acc6c888feee914ccf91cb65bdb71ca
mohansakireddy <mohansakireddy@gmail.com>
2024-11-04 12:37:44 +0530

    added mysql database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS TRANSACTION;
-DROP TABLE IF EXISTS CUSTOMER;
+DROP TABLE IF EXISTS retailer.TRANSACTION;
+DROP TABLE IF EXISTS retailer.customer;
 
 
-CREATE TABLE CUSTOMER (
+CREATE TABLE retailer.CUSTOMER (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE TRANSACTION (
+CREATE TABLE retailer.TRANSACTION (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     customer_id BIGINT ,
     transaction_date DATE NOT NULL,
@@ -16,19 +16,19 @@ CREATE TABLE TRANSACTION (
 );
 
 
---inserting data ito customer table
-INSERT INTO CUSTOMER (name) VALUES ('mohan');
-INSERT INTO CUSTOMER (name) VALUES ('siva');
+-- inserting data into customer table
+INSERT INTO retailer.CUSTOMER (name) VALUES ('mohan');
+INSERT INTO retailer.CUSTOMER (name) VALUES ('siva');
 
---inserting data into transaction table
---customer 1
-INSERT INTO TRANSACTION (customer_id, transaction_date, amount) VALUES (1,'2024-10-24',175);
-INSERT INTO TRANSACTION (customer_id, transaction_date, amount) VALUES (1,'2024-09-24',75);
-INSERT INTO TRANSACTION (customer_id, transaction_date, amount) VALUES (1,'2024-08-24',5);
+-- inserting data into transaction table
+-- customer 1
+INSERT INTO retailer.TRANSACTION (customer_id, transaction_date, amount) VALUES (1,'2024-10-24',175);
+INSERT INTO retailer.TRANSACTION (customer_id, transaction_date, amount) VALUES (1,'2024-09-24',75);
+INSERT INTO retailer.TRANSACTION (customer_id, transaction_date, amount) VALUES (1,'2024-08-24',5);
 
---transaction for customer 2
+-- transaction for customer 2
 
 
-INSERT INTO TRANSACTION (customer_id, transaction_date, amount) VALUES (2,'2024-10-24',175);
-INSERT INTO TRANSACTION (customer_id, transaction_date, amount) VALUES (2,'2024-09-24',75);
-INSERT INTO TRANSACTION (customer_id, transaction_date, amount) VALUES (2,'2024-05-24',155);
+INSERT INTO retailer.TRANSACTION (customer_id, transaction_date, amount) VALUES (2,'2024-10-24',175);
+INSERT INTO retailer.TRANSACTION (customer_id, transaction_date, amount) VALUES (2,'2024-09-24',75);
+INSERT INTO retailer.TRANSACTION (customer_id, transaction_date, amount) VALUES (2,'2024-05-24',155);
